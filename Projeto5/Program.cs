7a9a503717201a7767f167033f88c8b0f2c4a6ea
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-02-20 01:59:49 -0300

    Configurando Program.cs

--- Projeto5/Program.cs
+++ Projeto5/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Projeto5.Entities;
+
+var motor = new Motor("Flex", 140);
+
+var carro = new Carro("####", motor);
+
+var pessoa1 = new Pessoa("####", 00);
+var pessoa2 = new Pessoa("@@@@", 01, carro);
