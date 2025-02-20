a568f396db4241706dc24eb96acb5bd3a4eede47
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-02-20 01:51:27 -0300

    Modificando Class Carro

--- Projeto5/Entities/Carro.cs
+++ Projeto5/Entities/Carro.cs
@@ -7,7 +7,13 @@ namespace Projeto5.Entities;
 public class Carro : IVeiculo
 {
     public string Modelo { get; set; } = string.Empty;
+    public Motor? Motor {get;private set;}
 
+    public Carro(string modelo, Motor motor)
+    {
+        Modelo = modelo;
+        Motor = motor;
+    }
     public void Desligar()
     {
         Console.WriteLine("Carro Desligado!");
