a6ab915fa734c2a38160e3270c0c48a2b8b0d0a8
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-06 03:44:42 -0300

    Adicionando Projeto

---
+++ Projects/GetHashCode and Equals/Program.cs
@@ -0,0 +1,37 @@
+﻿using GetHashCode_and_Equals.Entities;
+
+namespace GetHashCode_and_Equals;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // resposta não é direta
+        // mais rápido
+        string a = "Olivio";
+        string b = "Olivio";
+
+        Console.WriteLine(a.GetHashCode());
+        Console.WriteLine(b.GetHashCode());
+
+        // resposta é direta
+        // mais lento
+        Console.WriteLine(a.Equals(b));
+
+        // teste
+        Client c1 = new Client()
+        {
+            Name = "Olivio",
+            Email = "a"
+        };
+        Client c2 = new Client()
+        {
+            Name = "Eu",
+            Email = "a"
+        };
+
+        Console.WriteLine(c1.Equals(c2));
+        Console.WriteLine(c1.GetHashCode());
+        Console.WriteLine(c2.GetHashCode());
+    }
+}
