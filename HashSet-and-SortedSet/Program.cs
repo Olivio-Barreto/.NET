9b4994aa036f33dfcfe0ed0a2ac31293b7ed08bb
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-23 02:17:17 -0300

    Interseção de coleções

--- HashSet-and-SortedSet/Program.cs
+++ HashSet-and-SortedSet/Program.cs
@@ -6,11 +6,17 @@ public class Program
     {
         // coolection expression
         SortedSet<int> a = [1, 2, 3, 4];
-        SortedSet<int> b = [5, 6, 7, 8];
+        SortedSet<int> b = [4, 5, 6, 7, 8];
 
         // union
         SortedSet<int> c = new SortedSet<int>(a);
         c.UnionWith(b);
+        PrintCollection(c);
+
+        // intersection
+        SortedSet<int> d = new SortedSet<int>(a);
+        d.IntersectWith(b);
+        PrintCollection(d);
     }
 
     static void PrintCollection<T>(IEnumerable<T> collection)
