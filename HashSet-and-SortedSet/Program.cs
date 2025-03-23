b8402d8ff8b00ae0f33519be6b6cbd9805c6a729
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-23 01:50:10 -0300

    Implementando método PrintCollection

---
+++ HashSet-and-SortedSet/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HashSet_and_SortedSet;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    static void PrintCollection<T>(IEnumerable<T> collection)
+    {
+        foreach (T obj in collection)
+        {
+            Console.WriteLine(obj + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
