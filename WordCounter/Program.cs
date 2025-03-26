db06ceebde8d2ec650e2c813e1492962da0f2a77
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-26 20:31:57 -0300

    Implementando contador de palavras

---
+++ WordCounter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Word_Counter;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    static int WordCounter(string message)
+    {
+        int count = 0;
+        string[] array = message.Split(' ');
+        for (int i = 0; i < array.Length; i++)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
