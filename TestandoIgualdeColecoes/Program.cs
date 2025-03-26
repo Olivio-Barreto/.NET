2683d3b4b5f77189961f7597b6b8c6e40087db15
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-26 16:51:22 -0300

    Teste v1

---
+++ TestandoIgualdeColecoes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestandoIgualdeColecoes
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("Fulano");
+            set.Add("Ciclano");
+
+            // verificando elementos se GetHashCode e Equals são implementados no tipo da coleção
+            Console.WriteLine(set.Contains("Fulano"));
+        }
+    }
+}
+
