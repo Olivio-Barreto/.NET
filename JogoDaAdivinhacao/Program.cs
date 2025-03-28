e5fff40c421645ff8d08875f353a9a73232d22d7
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-27 23:18:43 -0300

    Jogo da Adivinhacao

---
+++ JogoDaAdivinhacao/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace JogoDaAdivinhacao;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        int num = Random.Shared.Next(1, 100);
+        int count = 0;
+
+        while (true)
+        {
+            Console.Write("Digite o numero: ");
+            var numUser = int.Parse(Console.ReadLine()!);
+
+            if (numUser == num) break;
+
+            Console.WriteLine(numUser > num ? "É menor" : "É maior");
+
+            count++;
+        }
+
+        Console.WriteLine($"Você acertou com {count} tentativas. O número foi {num}");
+    }
+}
