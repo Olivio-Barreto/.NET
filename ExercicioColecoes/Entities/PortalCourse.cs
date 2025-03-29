7d1e798e1d8e0b73b07828311f86381ba3c1b526
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-03-29 16:00:53 -0300

    Modificando exercicio

--- ExercicioColecoes/Entities/PortalCourse.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace ExercicioColecoes.Entities;
-
-public class PortalCourse
-{
-    public string Name { get; set; } = null!;
-    public HashSet<User> Users { get; set; } = new HashSet<User>();
-}
--- ExercicioColecoes/Entities/User.cs
+++
@@ -1,7 +0,0 @@
-namespace ExercicioColecoes.Entities;
-
-public class User(string name, int id)
-{
-    public int Id { get; init; } = id;
-    public string Name { get; set; } = name;
-}
