3c8cd7a00693b92eed612846b3b859ebb3a7d735
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-04-02 17:23:42 -0300

    Treinando Dictionary

---
+++ Dictionary-and-SortedDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Dictionary_and_SortedDictionary;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // cookie
+        Dictionary<string, string> cookies = new();
+
+        cookies["user"] = "olivio";
+        cookies["email"] = "olivio@gmail.com";
+        cookies["phone"] = "123445677345";
+        cookies["phone"] = "345655373565";
+
+        Console.WriteLine(cookies["phone"]);
+        Console.WriteLine(cookies["email"]);
+
+        cookies.Remove("phone");
+
+        foreach (KeyValuePair<string, string> item in cookies)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+    }
+}
