70b6ffd759ced8f8fe4c12b2a87fcce40cbe56e7
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-04-03 08:31:10 -0300

    Testando metodo

---
+++ ExtensionsMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using ExtensionsMethods.Extensions;
+
+namespace ExtensionsMethods;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var temp = new DateTime(2025, 04, 03, 07, 30, 00);
+
+        Console.WriteLine(temp.ElapsedTime());
+    }
+}
